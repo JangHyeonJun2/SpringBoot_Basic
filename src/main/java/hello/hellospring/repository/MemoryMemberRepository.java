a5e7d29850379a8121c5b3c592ca56e4b075c77a
JangHyeonJun2 <epfvkdlxj@naver.com>
2020-09-12 17:22:52 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+
+@Repository
 public class MemoryMemberRepository implements MemberRepositroy {
 
     private static Map<Long, Member> store = new HashMap<>();
